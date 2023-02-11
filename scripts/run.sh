a4b98e99a24dec8d91c9c0c6eaa50031c56824af
Frocalolo <frocalolo@proton.me>
2023-02-12 12:35:46 +1300

    Keybinds and tags

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,29 @@
 
 xrdb merge ~/.Xresources 
 xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wall/gruv.png &
+
+#  Polkit agent
+exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# Enable Super Keys For Menu
+ksuperkey -e 'Super_L=Alt_L|F1' &
+
+# Restore wallpaper
+nitrogen --restore &
+
 xset r rate 200 50 &
+
+# Launch notification daemon
+dunst &
+
+# Launch compositor
 picom &
+
+# copyq
+exec copyq &
+
+# Fix Java problems
+wmname "LG3D"
+export _JAVA_AWT_WM_NONREPARENTING=1
 
 ~/.config/chadwm/scripts/bar.sh &
 while type dwm >/dev/null; do dwm && continue || break; done
